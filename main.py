b06ba81a59385b641db0890b51fec2791effee04
Kartik Neti <kartikprajak@gmail.com>
2023-10-24 20:20:17 +0530

    MVP

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from flask import Flask, render_template
+import pandas as pd
 
 
 app = Flask(__name__)
 
 
-
 @app.route("/")
 def meaning():
-    
+
     return render_template("dictionary.html")
+
 
 @app.route("/api/v1/<word>")
 def extract_meaning(word):
-    defi = word.upper()
+
+    df = pd.read_csv("dictionary.csv")
+
+    defi = df.loc[df["word"] == word]["definition"].squeeze()
     dictionary = {"word:": word,
                   "defintion: ": defi}
     return dictionary
-    
-    
+
 
 if __name__ == ("__main__"):
     app.run(debug=True)
-    
